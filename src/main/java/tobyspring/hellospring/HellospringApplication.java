3ba492779fa404e677967c5e9faa6f4bbf125012
Toby Lee <tobyilee@gmail.com>
2023-10-03 07:10:38 +1000

    프로젝트 생성

---
+++ src/main/java/tobyspring/hellospring/HellospringApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tobyspring/hellospring/HellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
