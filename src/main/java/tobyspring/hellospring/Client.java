b9d5ee0ff8b7c7a18c2a4d1a65cad70a0860aaa5
Toby Lee <tobyilee@gmail.com>
2023-12-30 20:55:51 +1000

    오브젝트 팩토리

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -5,7 +5,9 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService(new SimpleExRateProvider());
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
+
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
---
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -0,0 +1,11 @@
+package tobyspring.hellospring;
+
+public class ObjectFactory {
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
+}
