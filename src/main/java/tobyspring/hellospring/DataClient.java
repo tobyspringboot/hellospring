e2f3357f66564eb6dd175066dd6d47deed9d1063
Toby Lee <tobyilee@gmail.com>
2024-03-23 08:27:48 +1000

    DataAccessException과 예외 추상화

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -2,6 +2,9 @@ package tobyspring.hellospring;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 
@@ -11,15 +14,24 @@ public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
         OrderRepository repository = beanFactory.getBean(OrderRepository.class);
+        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-        Order order = new Order("100", BigDecimal.TEN);
-        repository.save(order);
+        try {
+            new TransactionTemplate(transactionManager).execute(status -> {
+                Order order = new Order("100", BigDecimal.TEN);
+                repository.save(order);
 
-        System.out.println(order);
+                System.out.println(order);
 
-        Order order2 = new Order("100", BigDecimal.ONE);
-        repository.save(order2);
+                Order order2 = new Order("100", BigDecimal.ONE);
+                repository.save(order2);
 
+                return null;
+            });
+        }
+        catch(DataIntegrityViolationException e) {
+            System.out.println("주문번호 중복 복구 작업");
 
+        }
     }
 }
--- src/main/java/tobyspring/hellospring/DataConfig.java
+++ src/main/java/tobyspring/hellospring/DataConfig.java
@@ -1,11 +1,14 @@
 package tobyspring.hellospring;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import tobyspring.hellospring.data.OrderRepository;
@@ -35,7 +38,17 @@ public class DataConfig {
     }
 
     @Bean
-    public OrderRepository orderRepository(EntityManagerFactory emf) {
-        return new OrderRepository(emf);
+    public BeanPostProcessor persistenceAnnotationBeanPostProcessor() {
+        return new PersistenceAnnotationBeanPostProcessor();
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public OrderRepository orderRepository() {
+        return new OrderRepository();
     }
 }
